8cd551bdb82b16cad7bb6fbce01779e7cba0e8a0
masreed <bluekitdrummer@gmail.com>
2021-02-26 10:06:13 -0600

    implement compose entry and display on homepage

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const _ = require('lodash');
 const mongoose = require('mongoose');
 
 const port = 3000;
+const posts = [];
 
 // Express instance with EJS templates and package utilization
 app = express();
@@ -15,10 +16,35 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 
+//Temporary data to display
+const homeContent = 'This is home content to display.'
+
+
 // App home route
 app.get('/', function(req, res){
-    res.render('home.ejs');
+    res.render('home.ejs', {
+        posts: posts
+    });
 });
+
+
+// App compose route
+app.get('/compose', function(req, res){
+    res.render('compose.ejs');
+});
+
+// App compose post route
+app.post('/compose', function(req, res){
+
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postContent
+    }
+
+    posts.push(post);
+    res.redirect('/');
+});
+
 
 // App about route
 app.get('/about', function(req, res){
