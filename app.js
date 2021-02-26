52609589dd0aed9b36b958dd6a907984443d2845
masreed <bluekitdrummer@gmail.com>
2021-02-25 18:27:32 -0600

    Implement EJS partials & Bootstrap

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.get('/', function(req, res){
     res.render('home.ejs');
 });
 
+// App about route
+app.get('/about', function(req, res){
+    res.render('about.ejs');
+})
+
 
 // Listen for server connection
 app.listen(port, function(){
